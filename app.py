22e3ecd78e599ae6a9fda463e61b6a1ced4e7dbc
bronzanat <natanbronzatto@gmail.com>
2024-01-12 10:09:19 -0300

    Tables

--- app.py
+++ app.py
@@ -4,13 +4,22 @@ import plotly.express as px
 
 
 # Titulo
-st.set_page_config(page_title="graficando", 
+st.set_page_config(page_title="tabelas", 
                    page_icon="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Map-circle-blue.svg/1024px-Map-circle-blue.svg.png",
                    initial_sidebar_state="expanded", layout="wide"
                    )
 st.title('Contagem de Segmentos de Empresas')
 
-
+hidden = """
+            <style>
+            #MainMenu {visibility: hidden !important;}
+            .reportview-container .main footer {visibility: hidden;}
+            footer {visibility: hidden !important;}
+            footer {display: none !important;}
+            header {visibility: hidden !important;}
+            </style>
+            """
+st.markdown(hidden, unsafe_allow_html=True)
 
 top_segmentos = pd.read_csv('./dataset/top-segmentos.csv', sep=';', encoding='ISO-8859-1')
 top_segmentos = top_segmentos.sort_values(by='Contas', ascending=False)
@@ -19,18 +28,16 @@ top_rotatividade = top_rotatividade.sort_values(by='Média Mensal', ascending=Fa
 
 
 
-# Center
 table_top_segmentos, table_top_rotatividade = st.tabs(["Segmentos ", "Rotatividade"])
 
-
 with table_top_segmentos:
-    st.write("Contagem das contas abertas por segmento.")
-    st.dataframe(top_segmentos, hide_index=True, width=1200)
+    st.write("**Total de Contas:** Contagem das contas abertas por segmento.")
+    # Adiciona um gradiente de fundo à tabela com base nos valores da coluna 'Contas'
+    st.dataframe(top_segmentos, width=1200, hide_index=True)
 
 with table_top_rotatividade:
-    st.write("Média aritmédica da quantidade de contas abertas por cada segmento. Maior média indica maior rotatividade.")
-    st.dataframe(top_rotatividade, hide_index=True, width=1200)
+    st.write("**Médias:** Média aritmédica da quantidade de contas abertas por cada segmento de empresa. Maior média indica maior rotatividade.")
 
-
-
+    # Adiciona um gradiente de fundo à tabela com base nos valores da coluna 'Média Mensal'
+    st.dataframe(top_rotatividade, width=1200, hide_index=True)
 #python -m streamlit run app.py
